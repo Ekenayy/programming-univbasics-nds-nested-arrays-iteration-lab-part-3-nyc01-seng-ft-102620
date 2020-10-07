ff7db5586eecdd4e452733e66918a800b655aa23
Ekene N <ekenayy@gmail.com>
2020-10-07 18:47:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index].length > -2
+    if src[row_index][element_index] > -2
       nil
     else 
     total += src[row_index][element_index]
